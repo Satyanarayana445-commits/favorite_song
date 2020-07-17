0cc5f73d1c506ddf0c87b370cda49c2ee48d480b
satya <venkatasatyanarayanakonisi@gmail.com>
2020-07-17 12:16:01 +0530

    first commit

---
+++ moviesonglinks/testapp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from testapp.models import Movie
+from testapp.forms import MovieForm
+def h1(request):
+    return render(request,"testapp/index.html")
+def h2(request):
+    form=MovieForm()
+    if request.method=="POST":
+        form=MovieForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return h1(request)
+    return render(request,"testapp/addmovie.html",{"form":form})
+def h3(request):
+    m1=Movie.objects.all()
+    return render(request,"testapp/movielink.html",{"m1":m1})
+def h4(request,id):
+    s1=Movie.objects.get(id=id);
+    s1.delete()
+    return h3(request)
